f3f8e11c6e8e993601ab49bf675b4b8f758107c6
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-10-23 18:01:44 -0400

    0.0.75

--- be-reformable.js
+++ be-reformable.js
@@ -3,4 +3,21 @@ import { BE } from 'be-enhanced/BE.js';
 import { propInfo } from 'be-enhanced/cc.js';
 
 /** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
-/** @import {Actions, PAP,  AP, BAP} from './ts-refs/be-calculating/types' */;
+/** @import {Actions, PAP,  AP, BAP} from './ts-refs/be-reformable/types' */;
+
+/**
+ * @implements {Actions}
+ * 
+ * 
+ * 
+ */
+class BeReformable extends BE {
+    /**
+     * @type {BEConfig<BAP, Actions & IEnhancement, any>}
+     */
+    static config = {
+    }
+}
+
+await BeReformable.bootUp();
+export {BeReformable};
