18ed30b45e7a8e84970a345b7555eead4746b600
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-11-16 11:01:41 -0500

    Merge branch 'baseline' of https://github.com/bahrus/be-reformable into baseline

--- be-reformable.js
+++ be-reformable.js
@@ -37,6 +37,7 @@ class BeReformable extends BE {
             when_updateOn_changes_invoke_hydrate: 0,
             when_path_changes_invoke_parsePath: 0,
             when_baseLink_changes_invoke_resolveBaseLink: 0,
+            when_fetchOptions_changes_invoke_suggestFetch: 0,
         },
         positractions: [resolved, rejected],
         actions:{
@@ -93,7 +94,7 @@ class BeReformable extends BE {
      * @param {BAP} self 
      */
     async updateAction(self){
-        const {enhancedElement, urlBuilder, baseURL, headerFields} = self;
+        const {enhancedElement, urlBuilder, baseURL, headerFields, headers} = self;
         if(!enhancedElement.checkValidity()) return {};
         const pathBuilder = [baseURL];
         const {tokens} = urlBuilder;
@@ -118,11 +119,21 @@ class BeReformable extends BE {
                 }
                 break;
         }
-        
+        /**
+         * @type {RequestInit}
+         */
+        const fetchOptions = {
+            method,
+            headers: {
+                ...(headers || {}),
+
+            }
+            
+        };
         
         enhancedElement.dispatchEvent(new BeFetchingEvent(url));
         return /** @type {PAP} */({
-            
+            fetchOptions
         });
     }
 
@@ -180,9 +191,9 @@ class BeReformable extends BE {
      * @param {BAP} self 
      */
     suggestFetch(self){
-        const {enhancedElement} = self;
+        const {enhancedElement, fetchOptions} = self;
         const {action} = enhancedElement;
-        enhancedElement.dispatchEvent(new BeFetchingEvent(action));
+        enhancedElement.dispatchEvent(new BeFetchingEvent(action, fetchOptions));
     }
 
 
