270973d59e5a4c6bb4198b2120ea03348a5e1c0b
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-11-17 10:05:08 -0500

    Merge branch 'baseline' of https://github.com/bahrus/be-reformable into baseline

---
+++ tests/NewtonMicroservice/GlobalThis.spec.mjs
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+test('test1', async ({ page }) => {
+    await page.goto('./tests/NewtonMicroservice/GlobalThis.html');
+    // wait for 1 second
+    await page.waitForTimeout(1500);
+    const editor = page.locator('#target');
+    await expect(editor).toHaveAttribute('mark', 'good');
+});
